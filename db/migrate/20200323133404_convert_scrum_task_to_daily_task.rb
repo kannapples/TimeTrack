76995736c5b6519ace15bf3489e654f65b04feea
Claire Kannapell <ckannapell7@gmail.com>
2020-04-15 00:56:26 -0500

    fix heroku db errors

--- db/migrate/20200323133404_convert_scrum_task_to_daily_task.rb
+++ db/migrate/20200323133404_convert_scrum_task_to_daily_task.rb
@@ -6,7 +6,8 @@ class ConvertScrumTaskToDailyTask < ActiveRecord::Migration[5.2]
   	# rename_index :daily_tasks, :index_scrum_tasks_on_project_module_id, :index_scrum_tasks_on_weekly_goal_id
   	rename_column :trackers, :project_module_id, :weekly_goal_id
   	rename_column :trackers, :scrum_task_id, :daily_task_id
-  	rename_index :trackers, :index_trackers_on_project_module_id, :index_trackers_on_weekly_goal_id
+  	# rename_index :trackers, :index_trackers_on_project_module_id, :index_trackers_on_weekly_goal_id
+  	add_index :trackers, :weekly_goal_id
   	rename_index :trackers, :index_trackers_on_scrum_task_id, :index_trackers_on_daily_task_id
   end
 end
