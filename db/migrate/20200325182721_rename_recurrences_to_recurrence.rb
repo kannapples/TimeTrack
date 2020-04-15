02d1c7df6993a401ec335e78c1d895d534ebb7a5
Claire Kannapell <ckannapell7@gmail.com>
2020-04-15 01:02:19 -0500

    fix heroku db errors

--- db/migrate/20200325182721_rename_recurrences_to_recurrence.rb
+++ db/migrate/20200325182721_rename_recurrences_to_recurrence.rb
@@ -1,7 +1,7 @@
 class RenameRecurrencesToRecurrence < ActiveRecord::Migration[5.2]
   def change
   	rename_column :daily_tasks, :repeat_recurrences_id, :repeat_recurrence_id
-  	rename_index :daily_tasks, :index_daily_tasks_on_repeat_recurrences_id, :index_daily_tasks_on_repeat_recurrence_id
+  	# rename_index :daily_tasks, :index_daily_tasks_on_repeat_recurrences_id, :index_daily_tasks_on_repeat_recurrence_id
   	rename_column :weekly_goals, :repeat_recurrences_id, :repeat_recurrence_id
   	rename_index :weekly_goals, :index_weekly_goals_on_repeat_recurrences_id, :index_weekly_goals_on_repeat_recurrence_id
 
