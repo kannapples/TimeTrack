d04ccc3a74acc4f5ef4f2d7cd0878b8c7e5b700c
Claire Kannapell <ckannapell7@gmail.com>
2020-04-15 00:49:56 -0500

    fix heroku db errors

--- db/migrate/20190824134056_fix_many_to_one_relationships.rb
+++ db/migrate/20190824134056_fix_many_to_one_relationships.rb
@@ -1,6 +1,6 @@
 class FixManyToOneRelationships < ActiveRecord::Migration[5.2]
   def change
-  	drop_table :projects_project_modules
+  	# drop_table :projects_project_modules
 
   	add_column :project_modules, :project_id, :integer
     add_index  :project_modules, :project_id
