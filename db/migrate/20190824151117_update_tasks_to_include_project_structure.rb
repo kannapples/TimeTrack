4fbed1bee6325915839b3b343a97b2d9e7c58311
Claire Kannapell <ckannapell7@gmail.com>
2020-04-15 00:53:24 -0500

    fix heroku db errors

--- db/migrate/20190824151117_update_tasks_to_include_project_structure.rb
+++ db/migrate/20190824151117_update_tasks_to_include_project_structure.rb
@@ -4,6 +4,6 @@ class UpdateTasksToIncludeProjectStructure < ActiveRecord::Migration[5.2]
     add_index  :scrum_tasks, :project_id 
 
     add_column :scrum_tasks, :project_module_id, :integer
-    add_index :scrum_tasks, :project_module_id
+    # add_index :scrum_tasks, :project_module_id
   end
 end
