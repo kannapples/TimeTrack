fb86cb4e82450760ad7d760339c38a0a718267ed
Claire Kannapell <ckannapell7@gmail.com>
2020-04-15 01:01:05 -0500

    fix heroku db errors

--- db/migrate/20200323174233_change_project_umbr_col_name.rb
+++ db/migrate/20200323174233_change_project_umbr_col_name.rb
@@ -1,6 +1,6 @@
 class ChangeProjectUmbrColName < ActiveRecord::Migration[5.2]
   def change
   	rename_column :weekly_goals, :project_umbrellas_id, :project_umbrella_id
-  	rename_index :weekly_goals, 'index_weekly_goals_on_project_umbrellas_id', 'index_weekly_goals_on_project_umbrella_id'
+  	# rename_index :weekly_goals, 'index_weekly_goals_on_project_umbrellas_id', 'index_weekly_goals_on_project_umbrella_id'
   end
 end
