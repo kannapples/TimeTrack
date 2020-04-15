9449a519129f4794f93ae6e93a8b765991ee5d0a
Claire Kannapell <ckannapell7@gmail.com>
2020-04-15 00:45:06 -0500

    fix error with migration

--- db/migrate/20190823155715_create_projects_project_modules_join_table.rb
+++ db/migrate/20190823155715_create_projects_project_modules_join_table.rb
@@ -1,8 +1,8 @@
-class CreateProjectsProjectModulesJoinTable < ActiveRecord::Migration[5.2]
-  def change
-  	create_join_table :projects, :project_modules do |t|
-    	t.index :projects      # add indexes for faster querying
-    	t.index :project_module_id
-    end
-  end
-end
+# class CreateProjectsProjectModulesJoinTable < ActiveRecord::Migration[5.2]
+#   def change
+#   	create_join_table :projects, :project_modules do |t|
+#     	t.index :projects      # add indexes for faster querying
+#     	t.index :project_module_id
+#     end
+#   end
+# end
